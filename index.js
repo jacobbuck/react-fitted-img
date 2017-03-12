e63ac171e36fcfd794e0c7b821d92bf2b19a8085
Jacob Buck <buck.jacob@gmail.com>
2017-03-12 18:15:59 +1300

    Tabs to 2 spaces

--- index.js
+++ index.js
@@ -6,57 +6,57 @@ var omit = require('lodash/omit');
 var React = require('react');
 
 var supports = window.CSS &&
-	CSS.supports &&
-	CSS.supports('object-fit', 'cover') &&
-	CSS.supports('object-position', '0 0');
+  CSS.supports &&
+  CSS.supports('object-fit', 'cover') &&
+  CSS.supports('object-position', '0 0');
 
 function FittedImg(props) {
-	var restProps = omit(props, keys(FittedImg.propTypes));
+  var restProps = omit(props, keys(FittedImg.propTypes));
 
-	return supports ?
-		React.createElement('img', assign({}, restProps, {
-			alt: props.alt,
-			height: props.height,
-			src: props.src,
-			style: assign({}, props.style, {
-				objectFit: props.fit,
-				objectPosition: props.position
-			}),
-			width: props.width
-		})) :
-		React.createElement('span', assign({}, restProps, {
-			'aria-label': props.alt,
-			role: 'img',
-			style: assign({}, props.style, {
-				backgroundImage: 'url("' + props.src + '")',
-				backgroundPosition: props.position,
-				backgroundSize: props.fit.replace('fill', '100% 100%').replace('none', 'auto'),
-				display: 'inline-block',
-				height: props.height ? props.height + 'px' : null,
-				width: props.width ? props.width + 'px' : null
-			})
-		}));
+  return supports ?
+    React.createElement('img', assign({}, restProps, {
+      alt: props.alt,
+      height: props.height,
+      src: props.src,
+      style: assign({}, props.style, {
+        objectFit: props.fit,
+        objectPosition: props.position
+      }),
+      width: props.width
+    })) :
+    React.createElement('span', assign({}, restProps, {
+      'aria-label': props.alt,
+      role: 'img',
+      style: assign({}, props.style, {
+        backgroundImage: 'url("' + props.src + '")',
+        backgroundPosition: props.position,
+        backgroundSize: props.fit.replace('fill', '100% 100%').replace('none', 'auto'),
+        display: 'inline-block',
+        height: props.height ? props.height + 'px' : null,
+        width: props.width ? props.width + 'px' : null
+      })
+    }));
 };
 
 FittedImg.propTypes = {
-	alt: React.PropTypes.string,
-	fit: React.PropTypes.oneOf(['fill', 'contain', 'cover', 'none']),
-	height: React.PropTypes.oneOfType([
-		React.PropTypes.string,
-		React.PropTypes.number
-	]),
-	position: React.PropTypes.string,
-	src: React.PropTypes.string.isRequired,
-	style: React.PropTypes.object,
-	width: React.PropTypes.oneOfType([
-		React.PropTypes.string,
-		React.PropTypes.number
+  alt: React.PropTypes.string,
+  fit: React.PropTypes.oneOf(['fill', 'contain', 'cover', 'none']),
+  height: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.number
+  ]),
+  position: React.PropTypes.string,
+  src: React.PropTypes.string.isRequired,
+  style: React.PropTypes.object,
+  width: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.number
   ]),
 };
 
 FittedImg.defaultProps = {
-	fit: 'fill',
-	position: '50% 50%'
+  fit: 'fill',
+  position: '50% 50%'
 };
 
 module.exports = FittedImg;
